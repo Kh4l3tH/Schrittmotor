2be7e26d3011a666eec2b978a8395a01058a3b08
Kh4l3tH <werther.b@gmail.com>
2016-02-22 13:41:42 +0100

    Replaced log with print

--- Schrittmotor.py
+++ Schrittmotor.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from SchrittmotorConst import *
-from lib.log import log
 from time import sleep
 from time import time
 
@@ -73,7 +72,7 @@ class Schrittmotor():
         self.cmd.setDirection(RECHTS)
         self.cmd.setMaxFrequency(frequency)
         self.cmd.setSteps(steps)
-        log('Motor {0}: \033[92m{1:b}\033[0m'.format(self.id, self.status()))
+        print 'Motor {0}: \033[92m{1:b}\033[0m'.format(self.id, self.status())
         self.cmd.startTravelProfile()
 
     def move_rel(self, distance, speed=2000, inverted=None):
@@ -116,23 +115,23 @@ class Schrittmotor():
         return self.cmd.getStatusByte()
 
     def wait(self):
-        log('Waiting for Motor {0}'.format(self.id))
+        print 'Waiting for Motor {0}'.format(self.id)
         self.cmd.setMotor(self.id)
         time_current = time()
         position_current = self.cmd.getPosition()
         while not self.cmd.isMotorReady():
             if time() - time_current >= 5:
-                log('Motor: {0}, Position: {1:.6f}, Status: \033[96m{2:b}\033[0m'.format(self.id, self.get_position(), self.status()))
+                print 'Motor: {0}, Position: {1:.6f}, Status: \033[96m{2:b}\033[0m'.format(self.id, self.get_position(), self.status())
                 time_current = time()
                 if position_current == self.cmd.getPosition():
-                    log('\033[91mMotor ist vermutlich stehen geblieben!\033[0m')
+                    print '\033[91mMotor ist vermutlich stehen geblieben!\033[0m'
                     raise ValueError('Motor ist vermutlich stehen geblieben!')
                 position_current = self.cmd.getPosition()
             sleep(0.1)
 
-        log('Checking Position Error:')
+        print 'Checking Position Error:'
         self.position_error()
-        log('Continuing')
+        print 'Continuing'
 
     def ready(self):
         self.cmd.setMotor(self.id)
@@ -143,11 +142,11 @@ class Schrittmotor():
         pos_control = self.cmd.getPosition()
         pos_encoder = self.cmd.getEncoderRotary()
         diff = pos_encoder-pos_control
-        log('Position   Motor: {0}'.format(pos_control))
-        log('Position Encoder: {0}'.format(pos_encoder))
+        print 'Position   Motor: {0}'.format(pos_control)
+        print 'Position Encoder: {0}'.format(pos_encoder)
         if abs(diff) < 3:
-            log('Position Diff: \033[92m{0}\033[0m'.format(diff))
+            print 'Position Diff: \033[92m{0}\033[0m'.format(diff)
         else:
-            log('Position Diff: \033[91m{0}\033[0m'.format(diff))
-            log('\033[91mPositionsfehler!\033[0m')
+            print 'Position Diff: \033[91m{0}\033[0m'.format(diff)
+            print '\033[91mPositionsfehler!\033[0m'
             #raise ValueError('Positionsfehler!')
